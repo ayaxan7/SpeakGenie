adeddf169080f2093adff67a200d56153cadc858
Syed Mohammad Ayaan <syedayaan9376@gmail.com>
2025-09-03 04:07:56 +0530

    Add Floating Action Button to HomeScreen for premium access

--- composeApp/src/commonMain/kotlin/com/ayaan/speakgenie/presentation/homescreen/HomeScreen.kt
+++ composeApp/src/commonMain/kotlin/com/ayaan/speakgenie/presentation/homescreen/HomeScreen.kt
@@ -10,9 +10,14 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.FloatingActionButtonDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -38,6 +43,7 @@ import com.ayaan.speakgenie.presentation.navigation.components.BottomNavigationB
 import org.jetbrains.compose.resources.painterResource
 import speakgenie.composeapp.generated.resources.Res
 import speakgenie.composeapp.generated.resources.boy
+import speakgenie.composeapp.generated.resources.fab
 
 @Composable
 fun HomeScreen(navController: NavController = rememberNavController()) {
@@ -55,8 +61,24 @@ fun HomeScreen(navController: NavController = rememberNavController()) {
     Scaffold(
         bottomBar = {
             BottomNavigationBar(navController)
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = { /* TODO: Handle FAB click */ },
+                shape = CircleShape,
+                containerColor = Color.Transparent,
+                elevation = FloatingActionButtonDefaults.elevation(0.dp)
+            ) {
+                Image(
+                    painter = painterResource(Res.drawable.fab),
+                    contentDescription = "Go Premium",
+                    modifier = Modifier.size(100.dp),
+                    contentScale = ContentScale.Crop
+                )
+            }
         }
-    ) { innerPadding ->
+    )
+    { innerPadding ->
         Column(
             modifier = Modifier
                 .fillMaxSize()
